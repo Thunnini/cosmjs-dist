9cd99f43defec2137558378f96dcb35cd219fa50
Thunnini <yunjh1994@gmail.com>
2020-08-27 02:12:36 +0900

    Fix the folder structure

---
+++ types/index.d.ts
@@ -0,0 +1,111 @@
+import * as logs from "./logs";
+export { logs };
+export { pubkeyToAddress, rawSecp256k1PubkeyToAddress } from "./address";
+export { Coin, coin, coins, parseCoins } from "./coins";
+export {
+  Account,
+  assertIsBroadcastTxSuccess,
+  Block,
+  BlockHeader,
+  CosmosClient,
+  GetSequenceResult,
+  IndexedTx,
+  isBroadcastTxFailure,
+  isBroadcastTxSuccess,
+  BroadcastTxFailure,
+  BroadcastTxResult,
+  BroadcastTxSuccess,
+  SearchByHeightQuery,
+  SearchByIdQuery,
+  SearchBySentFromOrToQuery,
+  SearchByTagsQuery,
+  SearchTxQuery,
+  SearchTxFilter,
+  isSearchByHeightQuery,
+  isSearchByIdQuery,
+  isSearchBySentFromOrToQuery,
+  isSearchByTagsQuery,
+} from "./cosmosclient";
+export { makeSignBytes } from "./encoding";
+export { buildFeeTable, FeeTable, GasLimits, GasPrice } from "./gas";
+export {
+  AuthAccountsResponse,
+  AuthExtension,
+  BankBalancesResponse,
+  BankExtension,
+  BaseAccount,
+  BlockResponse,
+  BroadcastMode,
+  DistributionCommunityPoolResponse,
+  DistributionDelegatorRewardResponse,
+  DistributionDelegatorRewardsResponse,
+  DistributionExtension,
+  DistributionParametersResponse,
+  DistributionValidatorOutstandingRewardsResponse,
+  DistributionValidatorResponse,
+  DistributionValidatorRewardsResponse,
+  DistributionWithdrawAddressResponse,
+  EncodeTxResponse,
+  GovExtension,
+  GovParametersResponse,
+  GovProposalsResponse,
+  GovProposalResponse,
+  GovProposerResponse,
+  GovDepositsResponse,
+  GovDepositResponse,
+  GovTallyResponse,
+  GovVotesResponse,
+  GovVoteResponse,
+  LcdApiArray,
+  LcdClient,
+  MintAnnualProvisionsResponse,
+  MintExtension,
+  MintInflationResponse,
+  MintParametersResponse,
+  NodeInfoResponse,
+  normalizeLcdApiArray,
+  normalizePubkey,
+  BroadcastTxsResponse,
+  SearchTxsResponse,
+  setupAuthExtension,
+  setupBankExtension,
+  setupDistributionExtension,
+  setupGovExtension,
+  setupMintExtension,
+  setupSlashingExtension,
+  setupStakingExtension,
+  setupSupplyExtension,
+  SlashingExtension,
+  SlashingParametersResponse,
+  SlashingSigningInfosResponse,
+  StakingExtension,
+  StakingParametersResponse,
+  StakingPoolResponse,
+  SupplyExtension,
+  TxsResponse,
+  uint64ToNumber,
+  uint64ToString,
+} from "./lcdapi";
+export { isMsgDelegate, isMsgSend, Msg, MsgDelegate, MsgSend } from "./msgs";
+export { InProcessOnlineSigner, OnlineSigner, SignRequest } from "./onlinesigner";
+export {
+  decodeAminoPubkey,
+  decodeBech32Pubkey,
+  encodeAminoPubkey,
+  encodeBech32Pubkey,
+  encodeSecp256k1Pubkey,
+} from "./pubkey";
+export { findSequenceForSignedTx } from "./sequence";
+export { encodeSecp256k1Signature, decodeSignature } from "./signature";
+export { CosmosFeeTable, SigningCosmosClient } from "./signingcosmosclient";
+export { isStdTx, pubkeyType, CosmosSdkTx, PubKey, StdFee, StdSignature, StdTx } from "./types";
+export {
+  AccountData,
+  Algo,
+  PrehashType,
+  OfflineSigner,
+  makeCosmoshubPath,
+  executeKdf,
+  KdfConfiguration,
+} from "./wallet";
+export { extractKdfConfiguration, Secp256k1Wallet } from "./secp256k1wallet";
